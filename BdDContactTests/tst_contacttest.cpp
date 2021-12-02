39faec014c6bfe7ed97e8f1f93ee59fda6dcf28e
peutt <aymericqueret@gmail.com>
2021-12-02 11:37:32 +0100

    test_Update

--- BdDContactTests/tst_contacttest.cpp
+++ BdDContactTests/tst_contacttest.cpp
@@ -52,21 +52,32 @@ void ContactTest::cleanupTestCase()
 void ContactTest::test_case1(QStringList requestList)
 {
     QString string;
+    if(_testState==3){
+        for(int i=0; i<requestList.size(); i++)
+        {
+            string += requestList[i];
+        }
+        qDebug() <<__FUNCTION__ <<"TestUpdate "<< string;
+        QCOMPARE(string, "2Test76f29450-aeda-4144-b71f-3bed815596b1TestAllisTestWattenbergTestAllis.Wattenberg@yopmail.comTest7296985TestWorkToulouseTest1999-04-24FRTestBlacklistTestNVidia");
+        _testState=4;
+    }
     if(_testState==2){
         for(int i=0; i<requestList.size(); i++)
         {
             string += requestList[i];
         }
-        _testState=3;
-        qDebug() <<__FUNCTION__ <<"Test2 "<< string;
+        qDebug() <<__FUNCTION__ <<"TestDelete "<< string;
         QCOMPARE(string, "");
+        _testState=3;
+        dbManager->updateData({"company"}, {"TestNVidia"}, {"country", "city"}, {"FR", "Toulouse"});
+        dbManager->requestData("company","TestNVidia");
     }
     if(_testState==1){
         for(int i=0; i<requestList.size(); i++)
         {
             string += requestList[i];
         }
-        qDebug() <<__FUNCTION__ <<"Test1 "<< string;
+        qDebug() <<__FUNCTION__ <<"TestInsert "<< string;
         QCOMPARE(string, "1TestGUIDfirstnamelastnameemailtelcategorycitybirth_daycountrylistcompany");
         dbManager->deleteData("id","1");
         _testState=2;
